97645d444196ab07b7f89a65ed77bdf17b57b9cb
ssinghbi02 <ssinghbi02@gmail.com>
2016-05-24 07:44:23 +0100

    Initial Commit

--- bitcoin/src/test/java/com/cs/rfq/service/RfqServiceImplTest.java
+++ bitcoin/src/test/java/com/cs/rfq/service/RfqServiceImplTest.java
@@ -3,7 +3,6 @@ package com.cs.rfq.service;
 import com.cs.rfq.model.Direction;
 import com.cs.rfq.model.Order;
 import com.cs.rfq.model.Quote;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,7 +13,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by ssinghbi02 on 24/05/2016.
@@ -29,7 +30,7 @@ public class RfqServiceImplTest {
 
     @Before
     public void setUp() {
-        when(liveOrderBoard.ordersFor(TEST_CURRENCY)).thenReturn(getOrders());
+        when(liveOrderBoard.ordersFor(TEST_CURRENCY)).thenReturn(getMockOrders());
     }
 
 
@@ -37,15 +38,15 @@ public class RfqServiceImplTest {
     public void testQuoteForWhenBothBuyAndSellMatchingClientOfferAvailable() {
         RfqService rfqService = new RfqServiceImpl(liveOrderBoard);
         Quote quote = rfqService.quoteFor(TEST_CURRENCY, 200).get();
-        Assert.assertTrue(quote.bid == 232.69);
-        Assert.assertTrue(quote.ask == 232.75);
+        assertThat(quote.bid, is(232.69));
+        assertThat(quote.ask, is(232.75));
     }
 
     @Test
     public void testQuoteForWhenOnlyBidMatchingClientOfferAvailable() {
         RfqService rfqService = new RfqServiceImpl(liveOrderBoard);
         Optional<Quote> quote = rfqService.quoteFor(TEST_CURRENCY, 500);
-        Assert.assertTrue(!quote.isPresent());
+        assertThat(quote.isPresent(), is(Boolean.FALSE));
     }
 
 
@@ -53,18 +54,18 @@ public class RfqServiceImplTest {
     public void testQuoteForWhenOnlyAskMatchingClientOfferAvailable() {
         RfqService rfqService = new RfqServiceImpl(liveOrderBoard);
         Optional<Quote> quote = rfqService.quoteFor(TEST_CURRENCY, 300);
-        Assert.assertTrue(!quote.isPresent());
+        assertThat(quote.isPresent(), is(Boolean.FALSE));
     }
 
     @Test
     public void testQuoteForWhenNoMatchingClientOfferAvailable() {
         RfqService rfqService = new RfqServiceImpl(liveOrderBoard);
         Optional<Quote> quote = rfqService.quoteFor(TEST_CURRENCY, 123);
-        Assert.assertTrue(!quote.isPresent());
+        assertThat(quote.isPresent(), is(Boolean.FALSE));
     }
 
 
-    private List<Order> getOrders() {
+    private List<Order> getMockOrders() {
         List<Order> orders = new ArrayList<>();
         orders.add(new Order(Direction.BUY, 232.71, TEST_CURRENCY, 200));
         orders.add(new Order(Direction.SELL, 232.74, TEST_CURRENCY, 100));
